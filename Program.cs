f804b51e2b980c0e2b153bd64bb1d1e26775d6da
Mykie <mykie_chen@hotmail.ca>
2026-03-30 10:35:13 -0400

    done

--- Program.cs
+++ Program.cs
@@ -11,6 +11,14 @@ foreach (var file in files)
     Console.WriteLine(file);
 }
 
+// Find all *.txt files in the stores folder and its subfolders
+IEnumerable<string> allFilesInAllFolders = Directory.EnumerateFiles("stores", "*.txt", SearchOption.AllDirectories);
+
+foreach (var file in allFilesInAllFolders)
+{
+    Console.WriteLine(file);
+}
+
 // using System.IO;
 // using System.Collections.Generic;
 
