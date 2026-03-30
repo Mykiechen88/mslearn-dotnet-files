d7287706a0a1a119d4c4f440c2df2e42e25d235a
Mykie <mykie_chen@hotmail.ca>
2026-03-30 11:26:07 -0400

    done

--- Program.cs
+++ Program.cs
@@ -1,48 +1,48 @@
-﻿IEnumerable<string> listOfDirectories = Directory.EnumerateDirectories("stores");
+﻿// IEnumerable<string> listOfDirectories = Directory.EnumerateDirectories("stores");
 
-foreach (var dir in listOfDirectories) {
-    Console.WriteLine(dir);
-}
+// foreach (var dir in listOfDirectories) {
+//     Console.WriteLine(dir);
+// }
 
-IEnumerable<string> files = Directory.EnumerateFiles("stores");
+// IEnumerable<string> files = Directory.EnumerateFiles("stores");
 
-foreach (var file in files)
-{
-    Console.WriteLine(file);
-}
-
-// Find all *.txt files in the stores folder and its subfolders
-IEnumerable<string> allFilesInAllFolders = Directory.EnumerateFiles("stores", "*.txt", SearchOption.AllDirectories);
-
-foreach (var file in allFilesInAllFolders)
-{
-    Console.WriteLine(file);
-}
-
-// using System.IO;
-// using System.Collections.Generic;
-
-// var salesFiles = FindFiles("stores");
-
-// foreach (var file in salesFiles)
+// foreach (var file in files)
 // {
 //     Console.WriteLine(file);
 // }
 
-// IEnumerable<string> FindFiles(string folderName)
+// // Find all *.txt files in the stores folder and its subfolders
+// IEnumerable<string> allFilesInAllFolders = Directory.EnumerateFiles("stores", "*.txt", SearchOption.AllDirectories);
+
+// foreach (var file in allFilesInAllFolders)
 // {
-//     List<string> salesFiles = new List<string>();
-
-//     var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
-
-//     foreach (var file in foundFiles)
-//     {
-//         // The file name will contain the full path, so only check the end of it
-//         if (file.EndsWith("sales.json"))
-//         {
-//             salesFiles.Add(file);
-//         }
-//     }
-
-//     return salesFiles;
+//     Console.WriteLine(file);
 // }
+
+using System.IO;
+using System.Collections.Generic;
+
+var salesFiles = FindFiles("stores");
+
+foreach (var file in salesFiles)
+{
+    Console.WriteLine(file);
+}
+
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        // The file name will contain the full path, so only check the end of it
+        if (file.EndsWith("sales.json"))
+        {
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
+}
