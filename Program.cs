aa5b88444634a37874f4d0f5c0c195b48ca27677
Mykie <mykie_chen@hotmail.ca>
2026-03-30 17:24:47 -0400

    done

--- Program.cs
+++ Program.cs
@@ -19,32 +19,37 @@
 //     Console.WriteLine(file);
 // }
 
-using System.IO;
-using System.Collections.Generic;
+// using System.IO;
+// using System.Collections.Generic;
 
-var salesFiles = FindFiles("stores");
+// var salesFiles = FindFiles("stores");
 
-foreach (var file in salesFiles)
-{
-    Console.WriteLine(file);
-}
+// foreach (var file in salesFiles)
+// {
+//     Console.WriteLine(file);
+// }
 
-IEnumerable<string> FindFiles(string folderName)
-{
-    List<string> salesFiles = new List<string>();
+// IEnumerable<string> FindFiles(string folderName)
+// {
+//     List<string> salesFiles = new List<string>();
 
-    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+//     var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
 
-    foreach (var file in foundFiles)
-    {
-        // The file name will contain the full path, so only check the end of it
-        if (file.EndsWith("sales.json"))
-        {
-            salesFiles.Add(file);
-        }
-    }
+//     foreach (var file in foundFiles)
+//     {
+//         // The file name will contain the full path, so only check the end of it
+//         if (file.EndsWith("sales.json"))
+//         {
+//             salesFiles.Add(file);
+//         }
+//     }
 
-    return salesFiles;
-}
+//     return salesFiles;
+// }
 
 string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+Console.WriteLine($"stores{Path.DirectorySeparatorChar}201");
+Console.WriteLine(Path.GetExtension("sales.json"));
+string fileName = $"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales{Path.DirectorySeparatorChar}sales.json";
+FileInfo info = new FileInfo(fileName);
+Console.WriteLine($"Full Name: {info.FullName}{Environment.NewLine}Directory: {info.Directory}{Environment.NewLine}Extension: {info.Extension}{Environment.NewLine}Create Date: {info.CreationTime}"); // And many more
