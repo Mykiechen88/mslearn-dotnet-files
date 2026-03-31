21bc28fc62579b65caf7b61dab9377016bb60a5d
Mykie <mykie_chen@hotmail.ca>
2026-03-31 14:05:15 -0400

    done

--- Program.cs
+++ Program.cs
@@ -1,8 +1,44 @@
-﻿var currentDirectory = Directory.GetCurrentDirectory();
-var storesDirectory = Path.Combine(currentDirectory, "stores");
-var salesFiles = FindFiles(storesDirectory);
+﻿// Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "stores","201","newDir"));
+// Console.WriteLine("Directory created successfully.");
 
-foreach (var file in salesFiles)
+// bool doesDirectoryExist = Directory.Exists(filePath);
+
+// File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), "greeting.txt"), "Hello World!");
+
+// var currentDirectory = Directory.GetCurrentDirectory();
+// var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+// var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+// Directory.CreateDirectory(salesTotalDir);     
+// var salesFiles = FindFiles(storesDirectory);
+
+// File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
+
+// IEnumerable<string> FindFiles(string folderName)
+// {
+//     List<string> salesFiles = new List<string>();
+
+//     var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+//     foreach (var file in foundFiles)
+//     {
+//         var extension = Path.GetExtension(file);
+//         if (extension == ".json")
+//         {
+//             salesFiles.Add(file);
+//         }
+//     }
+
+//     return salesFiles;
+// }
+
+using Newtonsoft.Json;
+var salesJson = File.ReadAllText($"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales.json");
+var salesData = JsonConvert.DeserializeObject<SalesTotal>(salesJson);
+
+Console.WriteLine(salesData.Total);
+
+class SalesTotal
 {
-    Console.WriteLine(file);
+  public double Total { get; set; }
 }
